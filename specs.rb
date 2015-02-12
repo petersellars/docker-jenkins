500c3492b959b100852e6d9d7f307f30b70579ff
Pete Sellars <psellars@gmail.com>
2015-02-12 23:59:29 +1300

    Update image to reflect Docker Hub repository

--- specs.rb
+++ specs.rb
@@ -2,7 +2,7 @@ require 'docker'
 
 describe "Jenkins image" do
   before(:all) {
-    @image = Docker::Image.all().detect{|i| i.info['Repository'] == 'petersellars/jenkins'}
+    @image = Docker::Image.all().detect{|i| i.info['Repository'] == 'cato1971/docker-jenkins'}
   }
 
   it "should be availble" do
